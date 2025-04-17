e0923950796f9e8916fd16e3f25a3ea0c90d6b02
Gennadiy Farladanskiy <genasha@yahoo.com>
2025-04-16 21:40:59 -0700

    Final

--- docs/logic.js
+++ docs/logic.js
@@ -27,6 +27,10 @@ let initializeMap = () => {
     tilematrixset: 'GoogleMapsCompatible_Level'
   });
 
+  var worldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+  });
+
   // Add default layer and define base layers
   streetLayer.addTo(map);
   let baseMaps = {
